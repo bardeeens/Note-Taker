c9bbe932e2e01ee035ff63c573a26979ec2f1561
Bardini <derekbardini@gmail.com>
2021-02-03 20:29:58 -0600

    delete now works properly

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// required node modules
 var express = require("express");
 var path = require("path");
 var fs = require('fs')
@@ -8,20 +9,18 @@ var PORT = process.env.PORT || 3035;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var notes = [
-];
-
+// this allows us to use everything in the public folder(css, js)
 app.use(express.static('./02-Homework/Develop/public'))
 
-
+// gets us notes.html at /notes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/02-Homework/Develop/public/notes.html"));
   });
-
+// gets us db.json at /api/notes
   app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/02-Homework/Develop/db/db.json"));
   });
-
+// posts changes to db.json
   app.post("/api/notes", function(req, res) {
     let newNote = req.body
     let rawData = fs.readFileSync('./02-Homework/Develop/db/db.json')
@@ -35,27 +34,36 @@ app.get("/notes", function(req, res) {
       console.log("success");
     })
   });
-    app.delete("/api/notes/:notes", function(req, res){
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/02-Homework/Develop/public/index.html"));
+  });
+
+    app.delete("/api/notes/:id", function(req, res){
     let rawData = fs.readFileSync('./02-Homework/Develop/db/db.json')
     let result = JSON.parse(rawData)
       
-      var chosen = req.params.newNote
-      console.log(chosen);
+      var chosen = req.params.id
+      console.log(result);
       
       for (let i = 0; i < result.length; i++) {
-        if (result[i].id == result){
-          result.remove(result[i])
+        if (result[i].id == chosen){
+          result.splice(i, 1)
           console.log("its happening");
+          res.json(result)
         }
        
         
       }
-
+fs.writeFile(__dirname + "/02-Homework/Develop/db/db.json", JSON.stringify(result), function(error){
+      if (error) {
+        throw error
+      }
+      console.log("success");
+    })
     })
 
-    app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "/02-Homework/Develop/public/index.html"));
-    });
+   
   
    
 
